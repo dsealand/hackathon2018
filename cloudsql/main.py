bfef96875d26c56f71bd52ebcbc8618238a0c65e
hmc-hippo-fa18 <dsealand@cs.hmc.edu>
2018-11-03 04:05:52 -0700

    python interaction with mySQL

---
+++ cloudsql/main.py
@@ -0,0 +1,109 @@
+# Copyright 2013 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Sample App Engine application demonstrating how to connect to Google Cloud SQL
+using App Engine's native unix socket or using TCP when running locally.
+
+For more information, see the README.md.
+"""
+
+# [START gae_python_mysql_app]
+import os
+import urllib
+
+#from google.appengine.api import users
+#from google.appengine.ext import nbd
+
+import MySQLdb
+import webapp2
+import jinja2
+
+# JINJA_ENVIRONMENT = jinja2.Environment(
+#     loader=jinja2.FileSystemLoader(os.path.dirname(_file_)),
+#     extensions=['jinja2.ext.autoescape'],
+#     autoescape=True)
+
+# These environment variables are configured in app.yaml.
+CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
+
+
+def connect_to_cloudsql():
+    # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
+    # will be set to 'Google App Engine/version'.
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+        # Connect using the unix socket located at
+        # /cloudsql/cloudsql-connection-name.
+        cloudsql_unix_socket = os.path.join(
+            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+
+        db = MySQLdb.connect(
+            unix_socket=cloudsql_unix_socket,
+            user=CLOUDSQL_USER,
+            passwd=CLOUDSQL_PASSWORD)
+
+    # If the unix socket is unavailable, then try to connect using TCP. This
+    # will work if you're running a local MySQL server or using the Cloud SQL
+    # proxy, for example:
+    #
+    #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
+    #
+    else:
+        db = MySQLdb.connect(
+            host='127.0.0.1', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+
+    return db
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        """Simple request handler that shows all of the MySQL variables."""
+        self.response.headers['Content-Type'] = 'text/plain'
+
+        db = connect_to_cloudsql()
+
+        databaseName = 'clients'
+        tableName1 = 'c_1'
+        tableName2 = 'c_2'
+
+        cursor = db.cursor()
+        cursor.execute('SHOW VARIABLES')
+        cursor.execute('USE ' + databaseName)
+        cursor.execute('SELECT * FROM ' + tableName1)
+        cursor.execute('SELECT * FROM ' + tableName2)
+
+        for r in cursor.fetchall():
+           self.response.write('{}\n'.format(r))
+
+
+        # template_values = {
+        #     'user': user,
+        #     'greetings': greetings,
+        #     'guestbook_name': urllib.quote_plus(guestbook_name),
+        #     'url': url,
+        #     'url_linktext': url_linktext,
+        # }
+
+        # template = JINJA_ENVIRONMENT.get_template('index.html')
+        # self.response.write(template.render(template_values))
+
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
+
+# [END gae_python_mysql_app]
